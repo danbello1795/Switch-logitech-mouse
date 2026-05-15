cf4937cd5f1e1aeb33228068412236f4a7a9e1ec
danbello1795 <daniel.bello1795@gmail.com>
2026-05-15 17:26:33 -0600

    fix: Update feature byte for Logitech keyboard switching to channel 2

--- windows/switch_keyboard_mouse_to_channel_2.bat
+++ windows/switch_keyboard_mouse_to_channel_2.bat
@@ -3,7 +3,9 @@ rem Channel 1 = USB dongle on this laptop
 rem Channel 2 = Bluetooth on the other laptop
 
 rem Switch Logitech keyboard to channel 2
-.\hidapitester.exe --vidpid 046D:C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x01,0x09,0x1E,0x01,0x00,0x00
+rem MX Keys S uses feature byte 0x0A. Try the receiver keyboard slots seen on this laptop.
+.\hidapitester.exe --vidpid 046D:C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x01,0x0A,0x1E,0x01,0x00,0x00
+.\hidapitester.exe --vidpid 046D:C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x03,0x0A,0x1E,0x01,0x00,0x00
 
 rem Switch Logitech MX Master 3S to channel 2
 .\hidapitester.exe --vidpid 046D:C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x0A,0x1E,0x01,0x00,0x00
